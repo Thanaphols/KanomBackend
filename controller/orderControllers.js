1b3d4a6508253421658ac6a4d5f61e0e9a00e79f
Thanaphols <cupteo254504@gmail.com>
2025-05-03 22:19:17 +0700

    Third

--- controller/orderControllers.js
+++ controller/orderControllers.js
@@ -3,3 +3,8 @@ const mysql = require('../db')
 exports.getallOrder= (req, res)=>{
     res.send("All order")
 }
+
+exports.getOrderID= (req, res)=>{
+    const id = req.body.o_id
+    res.send("All order" + id)
+}
