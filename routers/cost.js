37f320f78f6252aa517241bc8264a74f81467846
Thanaphols <cupteo254504@gmail.com>
2026-01-12 21:25:49 +0700

    routerOrderID

--- routers/cost.js
+++ routers/cost.js
@@ -2,4 +2,5 @@ const express = require("express")
 const router = express.Router()
 const costControllers = require('../controllers/costControllers')
 router.get('/',costControllers.getCost)
+router.post('/addCost',costControllers.addCost)
 module.exports = router
