a0c275bfc39f9196e4423f769f14d3dee64b25d9
Thanaphols <cupteo254504@gmail.com>
2026-01-13 01:09:32 +0700

    DeleteCost

--- routers/cost.js
+++ routers/cost.js
@@ -4,4 +4,5 @@ const costControllers = require('../controllers/costControllers')
 router.get('/',costControllers.getCost)
 router.post('/addCost',costControllers.addCost)
 router.patch('/editCost',costControllers.editCost)
+router.delete('/deleteCost/:co_ID',costControllers.deleteCost)
 module.exports = router
