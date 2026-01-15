107890cf0bedd7dd37844b972753b707073f42e1
Thanaphols <cupteo254504@gmail.com>
2026-01-15 17:27:59 +0700

    store

--- routers/store.js
+++ routers/store.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const storeControllers = require('../controllers/storeControllers')
 
 router.get('/',storeControllers.getStore)
+router.post('/addStore',storeControllers.addStore)
 module.exports = router
 
 
